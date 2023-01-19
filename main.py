08717149ff58b131a13a8a218ea0b500dacfffb7
adamcardoso <adamjuniorcardoso@gmail.com>
2023-01-19 18:54:52 -0300

    Analyse and Evaluate the Results

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 import pandas
 from pandas import DataFrame
 import matplotlib.pyplot as plt
+from sklearn.linear_model import LinearRegression
 
 data = pandas.read_csv('cost_revenue_clean.csv')  # read the data
 print(data)  # print the data
@@ -12,8 +13,15 @@ y = DataFrame(data, columns=['worldwide_gross_usd'])  # get the worldwide gross
 # print(x)
 # print(y)
 
+regression = LinearRegression()  # create the linear regression model
+regression.fit(x, y)  # fit the model
+# Slope Coefficient:
+print('Slope Coefficient: ', regression.coef_)  # print the slope coefficient
+print('Intercept: ', regression.intercept_)  # print the intercept
+
 plt.figure(figsize=(10, 6))  # set the figure size
 plt.scatter(x, y, alpha=0.3)  # plot the data
+plt.plot(x, regression.predict(x), color='red', linewidth=4)  # plot the regression line
 plt.title('Film Cost vs Global Revenue')  # set the title
 plt.xlabel('Production Budget $')  # set the x label
 plt.ylabel('Worldwide Gross $')  # set the y label
@@ -21,3 +29,4 @@ plt.ylim(0, 3000000000)  # set the y limit
 plt.xlim(0, 450000000)  # set the x limit
 print(plt.show())  # show the plot
 
+print(regression.score(x, y))  # get the score
